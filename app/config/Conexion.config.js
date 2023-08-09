939beb9df04300b6f3eaeedeecc244b216957d47
ZrkllT <zero.kullt@gmail.com>
2023-08-08 22:35:02 -0400

    equipo 1 2do

--- app/config/Conexion.config.js
+++ app/config/Conexion.config.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 
-const conexion = new Sequelize("blog0023", "postgres", "asdasd", {
+const conexion = new Sequelize("blog0023", "postgres", "sasdasdas", {
     dialect: "postgres",
     host: "localhost",
     port: 5432
