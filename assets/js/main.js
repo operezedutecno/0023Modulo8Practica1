6b5a36849b41c6ca5f5a4940b574ea76de6d2a04
Osman Pérez <operez@edutecno.com>
2023-08-21 20:27:08 -0400

    Actualizar usuarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,8 @@ $(() => {
                         <td>${usuario.createdAt}</td>
                         <td>${usuario.updatedAt}</td>
                         <td>
-                            <img src="/public/img/icons/eliminar.png" class="enlace eliminar-usuario" data-id="${usuario.id}">
+                            <img src="/public/img/icons/edit.png" class="enlace actualizar-usuario" data-id="${usuario.id}" data-bs-toggle="modal" data-bs-target="#modal-actualizar-usuario">
+                            <img src="/public/img/icons/eliminar.png" class="enlace eliminar-usuario ms-2" data-id="${usuario.id}">
                         </td>
                     </tr>
                 `)
@@ -59,5 +60,52 @@ $(() => {
         })
     })
 
-    
+    $(document).on("click",".actualizar-usuario", function() {
+        const id = $(this).data("id")
+        fetch(`/usuarios/${id}`).then(resp => {
+            return resp.json()
+        }).then(response => {
+            // $("#modal-actualizar-usuario").show();
+            $("#txt-usuario").val(response.data.usuario)
+            $("#txt-id").val(response.data.id)
+        }).catch(error => {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Ocurrió un error consultado datos de usuario',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            })
+        })
+    })
+
+    $(document).on("submit", "#formulario-actualizacion", function(event) {
+        event.preventDefault()
+        const id = $("#txt-id").val();
+        const payload = {
+            usuario: $("#txt-usuario").val(),
+            contrasena: $("#txt-contrasena").val()
+        }
+        fetch(`/usuarios/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(payload),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        }).then(resp => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Éxito',
+                text: 'Usuario actualizado con éxito',
+                showConfirmButton: true,
+                confirmButtonText: 'Ok'
+            })
+            $("#modal-btn-close").trigger('click');
+            listarUsuarios();
+        }).catch(error => {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Ocurrió un error actualizando los datos de usuario',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            })
+        })
+    })
 })
